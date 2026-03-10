9b4077abcf533c191e934ac14bbbf9e9ed8afc55
Aleksei Khatkevich <qwerty12345@disroot.org>
2026-03-10 14:00:06 +0300

    43

--- user/db_model.py
+++ user/db_model.py
@@ -2,13 +2,13 @@ import uuid
 
 from database.postgres.base import Base
 from sqlalchemy.orm import Mapped, mapped_column
-from sqlalchemy import String
+from sqlalchemy import String, func
 
 
 class User(Base):
     __tablename__ = 'user'
 
-    id: Mapped[uuid.UUID] = mapped_column(primary_key=True, default=uuid.uuid7)
+    id: Mapped[uuid.UUID] = mapped_column(primary_key=True, server_default=func.uuidv7())
     name: Mapped[str] = mapped_column(String(30))
 
 
