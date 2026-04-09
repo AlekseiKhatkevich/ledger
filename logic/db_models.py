9273d27ada51c0343ae3c8da5c422f422ac3f90d
Aleksei Khatkevich <qwerty12345@disroot.org>
2026-04-09 16:38:53 +0300

    113

--- logic/db_models.py
+++ logic/db_models.py
@@ -4,7 +4,7 @@ import enum
 import uuid
 from typing import Annotated
 
-from sqlalchemy import BIGINT, Identity, String, ForeignKey, Computed
+from sqlalchemy import BIGINT, Identity, String, ForeignKey, Computed, Index, UniqueConstraint, CheckConstraint
 from sqlalchemy.dialects.postgresql import ENUM
 from sqlalchemy.orm import mapped_column, Mapped
 
@@ -16,11 +16,22 @@ class AssetOperationType(enum.StrEnum):
     SELL = 'SELL'
 
 
-# class UserAssetAddress(Base):
-#     pass
-#
-# class Ticker(Base):
-#     pass
+class UserAssetAddress(Base):
+    __tablename__ = 'user_asset_addresses'
+
+    id: Mapped[Annotated[int, mapped_column(BIGINT, Identity(always=True), primary_key=True)]]
+    public_key: Mapped[str]
+    wallet_name: Mapped[str | None]
+
+    def __repr__(self) -> str:
+        return f'{self.__class__.__name__}: {self.wallet_name = })'
+
+class AssetTicker(Base):
+    __tablename__ = 'asset_tickers'
+    name: Mapped[Annotated[str, mapped_column(String(10), primary_key=True)]]
+
+    def __repr__(self) -> str:
+        return f'{self.__class__.__name__}: {self.name = })'
 
 
 class UserAssetOperation(Base):
@@ -31,20 +42,39 @@ class UserAssetOperation(Base):
     type: Mapped[Annotated[enum.Enum, mapped_column(ENUM(AssetOperationType, name='asset_operation_type'))]]
     user_asset_id: Mapped[
         Annotated[int, mapped_column(ForeignKey('user_assets.id', ondelete='CASCADE'))]
-    ]
+    ] # index
     quantity: Mapped[decimal.Decimal]
     unit_price: Mapped[decimal.Decimal]
     summ: Mapped[Annotated[decimal.Decimal, mapped_column(Computed('unit_price * quantity'))]]
+    address: Mapped[
+        Annotated[int, mapped_column(ForeignKey('user_asset_address.id', ondelete='RESTRICT'))]
+    ]
 
     # parent = relationship("Parent", back_populates="children")
+
+    __table_args__ = (
+        Index('ix_user_asset_id', 'user_asset_id'),
+        CheckConstraint('quantity > 0', name='asset_qty_gt_0'),
+        CheckConstraint('unit+price > 0', name='unit_price_gt_0'),
+    )
+
+    def __repr__(self) -> str:
+        return f'{self.__class__.__name__}: {self.user_asset_id = })'
 
 class UserAsset(Base):
     __tablename__ = 'user_assets'
 
     id: Mapped[Annotated[int, mapped_column(BIGINT, Identity(always=True), primary_key=True)]]
     name: Mapped[str]
-    ticker: Mapped[Annotated[str, mapped_column(String(10))]]
-    user_id: Mapped[uuid.UUID]  # ticker + user unique together
+    ticker: Mapped[Annotated[str, mapped_column(ForeignKey('asset_tickers.name', ondelete='RESTRICT'))]]
+    user_id: Mapped[uuid.UUID]
+
+    __table_args__ = (
+        UniqueConstraint('user_id', 'ticker', ),
+    )
+
+    def __repr__(self) -> str:
+        return f'{self.__class__.__name__}: {self.name})'
 
     # children = relationship(
     #     "Child",
@@ -53,4 +83,6 @@ class UserAsset(Base):
     #     passive_deletes=True,
     # )
 
+    # todo
+    # @hybrid property
 
