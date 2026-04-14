3671e5fc374642a458fce614c1d008b2f2b5c163
Aleksei Khatkevich <qwerty12345@disroot.org>
2026-04-14 14:25:27 +0300

    125

--- tests/user/auth/test_keyckloak_middleware.py
+++ tests/user/auth/test_keyckloak_middleware.py
@@ -1,20 +1,25 @@
 from unittest.mock import Mock
 
+import msgspec
 import pytest
+from litestar import Litestar
 from litestar.connection import ASGIConnection
 from litestar.exceptions import NotAuthorizedException
 from litestar.middleware import AuthenticationResult
 
 from config import settings
-from main import app
-from user.auth.keycloack_middleware import KeyCloakAuthenticationMiddleware
+from user.auth.keycloack_middleware import KeyCloakAuthenticationMiddleware, JWTAuthenticationMiddleware
 from user.auth.keycloak_based import KeyCloakAuth
 from user.domain import KeyCloakToken, User
 
 
 @pytest.fixture
-def kc_auth_middleware(kc_auth: KeyCloakAuth) -> KeyCloakAuthenticationMiddleware:
+def kc_auth_middleware(kc_auth: KeyCloakAuth, app: Litestar) -> KeyCloakAuthenticationMiddleware:
     return KeyCloakAuthenticationMiddleware(auth_provider=kc_auth, app=app)
+
+@pytest.fixture
+def direct_jwt_auth_middleware(app: Litestar) -> JWTAuthenticationMiddleware:
+    return JWTAuthenticationMiddleware(app=app)
 
 @pytest.fixture
 def connection() -> Mock:
@@ -22,6 +27,26 @@ def connection() -> Mock:
     connection.headers = {settings.KEYCLOAK_API_KEY_HEADER: 'Bearer i_am_a_fake_jwt_token'}
     return connection
 
+
+async def test_direct_jwt_auth_middleware_negative_wrong_jwt(direct_jwt_auth_middleware, connection):
+    with pytest.raises(NotAuthorizedException, match='Wrong or damaged token'):
+        await direct_jwt_auth_middleware.authenticate_request(connection)
+
+async def test_direct_jwt_auth_middleware_positive(
+        direct_jwt_auth_middleware,
+        connection,
+        good_jwt_token_str,
+        kc_userinfo_response,
+):
+    connection.headers = {settings.KEYCLOAK_API_KEY_HEADER: f'Bearer {good_jwt_token_str}'}
+
+    auth_result = await direct_jwt_auth_middleware.authenticate_request(connection)
+
+    assert isinstance(auth_result, AuthenticationResult)
+    assert isinstance(auth_result.user, User)
+    assert isinstance(auth_result.auth, KeyCloakToken)
+    assert auth_result.user == msgspec.convert(kc_userinfo_response, User)
+    assert auth_result.auth.api_key == good_jwt_token_str
 
 async def test_kc_auth_middleware_negative_no_header(kc_auth_middleware, connection):
     connection.headers = {}
