cc8381aab8e65e56c18ee675de0981cb57fa03fc
Aleksei Khatkevich <qwerty12345@disroot.org>
2026-04-15 17:41:20 +0300

    tmp pytest 2

--- tests/logic/db_models/test_db_models.py
+++ tests/logic/db_models/test_db_models.py
@@ -1,11 +1,8 @@
 
 
 async def test_user_asset_address_positive(
-        # user_asset_address_factory,
         user_asset_address_in_db,
         pg_user_asset_repo,
-        monkeypatch
 ):
-    monkeypatch.setenv('PGDATABASE', 'ledger-db-test')
     address_from_db = await pg_user_asset_repo.get_by_pubkey(user_asset_address_in_db.public_key)
     assert address_from_db.as_fields_dict() == user_asset_address_in_db.as_fields_dict()
