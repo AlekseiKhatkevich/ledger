70fc8c167fb67be224212a44fc4bc1c2e1c32a6f
Aleksei Khatkevich <qwerty12345@disroot.org>
2026-04-16 17:05:36 +0300

    pytest savepoint tmp works 2

--- database/postgres/connection.py
+++ database/postgres/connection.py
@@ -3,7 +3,7 @@ import secrets
 import weakref
 from contextlib import asynccontextmanager, aclosing
 from functools import cache, cached_property
-from typing import TYPE_CHECKING, AsyncGenerator
+from typing import AsyncGenerator
 
 import anyio
 import structlog
@@ -11,9 +11,6 @@ from litestar.serialization import decode_json, encode_json
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession, AsyncConnection
 
 from config import settings
-
-if TYPE_CHECKING:
-    from sqlalchemy import URL
 
 log = structlog.get_logger()
 
@@ -24,41 +21,31 @@ __all__ = (
 
 @cache
 class DB:
-    def __init__(self, url: URL | None = None, finalize: bool = False) -> None:
+    def __init__(self, finalize: bool = False) -> None:
         self.outer_connection: AsyncConnection | None = None
-        # self.engine = create_async_engine(
-        #     url or settings.PG_DSN,
-        #     echo=settings.POSTGRES_ECHO,
-        #     echo_pool=settings.ECHO_POOL,
-        #     pool_pre_ping=settings.POOL_PRE_PING,
-        #     pool_timeout=settings.POOL_TIMEOUT,
-        #     pool_size=settings.POOL_SIZE,
-        #     max_overflow=settings.POOL_MAX_OVERFLOW,
-        #     pool_use_lifo=settings.POOL_USE_LIFO,
-        #     json_serializer=encode_json,
-        #     json_deserializer=decode_json,
-        #     execution_options={'logging_token': f'connect#: {secrets.token_hex(3)}',},
-        #     connect_args={'server_settings': {'application_name': f'{settings.APP_NAME}:{os.getpid()}'}},
-        # )
-        # if finalize:
-        #     self._finalizer = weakref.finalize(self, lambda: anyio.run(self.close))
+        if finalize:
+            self._finalizer = weakref.finalize(self, lambda: anyio.run(self.close))
 
-    @property
-    def engine(self):
+    @staticmethod
+    def _make_engine():
         return create_async_engine(
-        settings.PG_DSN,
-        echo=settings.POSTGRES_ECHO,
-        echo_pool=settings.ECHO_POOL,
-        pool_pre_ping=settings.POOL_PRE_PING,
-        pool_timeout=settings.POOL_TIMEOUT,
-        pool_size=settings.POOL_SIZE,
-        max_overflow=settings.POOL_MAX_OVERFLOW,
-        pool_use_lifo=settings.POOL_USE_LIFO,
-        json_serializer=encode_json,
-        json_deserializer=decode_json,
-        execution_options={'logging_token': f'connect#: {secrets.token_hex(3)}', },
-        connect_args={'server_settings': {'application_name': f'{settings.APP_NAME}:{os.getpid()}'}},
+            settings.PG_DSN,
+            echo=settings.POSTGRES_ECHO,
+            echo_pool=settings.ECHO_POOL,
+            pool_pre_ping=settings.POOL_PRE_PING,
+            pool_timeout=settings.POOL_TIMEOUT,
+            pool_size=settings.POOL_SIZE,
+            max_overflow=settings.POOL_MAX_OVERFLOW,
+            pool_use_lifo=settings.POOL_USE_LIFO,
+            json_serializer=encode_json,
+            json_deserializer=decode_json,
+            execution_options={'logging_token': f'connect#: {secrets.token_hex(3)}', },
+            connect_args={'server_settings': {'application_name': f'{settings.APP_NAME}:{os.getpid()}'}},
     )
+
+    @cached_property
+    def engine(self):
+        return self._make_engine()
 
     @asynccontextmanager
     async def make_outer_connection(self) -> AsyncGenerator[AsyncConnection]:
@@ -69,7 +56,8 @@ class DB:
         Makes outer transaction and joins current session inside it as a savepoint.
         This behavior is similar to Django test suit.
         """
-        self.outer_connection = await self.engine.connect()
+        outer_engine = self._make_engine()
+        self.outer_connection = await outer_engine.connect()
         transaction = await self.outer_connection.begin()
         try:
             yield self.outer_connection
@@ -78,17 +66,13 @@ class DB:
             await self.outer_connection.close()
             self.outer_connection = None
 
-    @property
+    @cached_property
     def _maker(self) -> async_sessionmaker:
-        maker = async_sessionmaker(
-            self.engine,
+        return async_sessionmaker(
+            bind=self.outer_connection or self.engine,
             expire_on_commit=False,
             join_transaction_mode='create_savepoint',
         )
-        if self.outer_connection is not None:
-            maker.configure(bind=self.outer_connection)
-
-        return maker
 
     @asynccontextmanager
     async def session(self) -> AsyncGenerator[AsyncSession]:
