a2a71b83b9b000b4b179a0bb5e42b694b45d11c7
Aleksei Khatkevich <qwerty12345@disroot.org>
2026-04-22 14:12:20 +0300

    148

--- etl/config.py
+++ etl/config.py
@@ -1,14 +1,34 @@
 from functools import cache
 
+from pydantic import HttpUrl
 from pydantic_settings import BaseSettings, SettingsConfigDict
+
+__all__ = (
+    'settings',
+)
+
+class TemporalSettings:
+    TEMPORAL_ADDRESS: str = 'temporal:7233'
+
+
+class ExternalUrlSettings:
+    EXTERNAL_URL_COINS_LIST: HttpUrl
 
 
 @cache
-class TemporalSettings(BaseSettings):
-    TEMPORAL_ADDRESS: str
-
+class Settings(
+    TemporalSettings,
+    ExternalUrlSettings,
+    BaseSettings,
+):
     model_config = SettingsConfigDict(
         env_ignore_empty=True,
+        env_file=('etl/secrets/ext_url/.env',  ),
         extra='ignore',
     )
 
+settings: Settings
+def __getattr__(name: str) -> Settings:
+    if name == 'settings':
+        return Settings()
+    raise AttributeError(f'Module {__name__} has no attribute {name}')
