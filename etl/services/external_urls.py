8d3a71c5a822e6fcfc62a7cca585aa5a5bc7963b
Aleksei Khatkevich <qwerty12345@disroot.org>
2026-04-22 17:13:01 +0300

    150

--- etl/services/external_urls.py
+++ etl/services/external_urls.py
@@ -1,16 +1,22 @@
 from functools import cache
+from typing import AsyncGenerator
 
 import httpx
+import ijson
 
 from config import settings
-from custom_types import CoinsListResponse
 
 
 @cache
 class ExternalUrlsService:
 
     @staticmethod
-    async def get_coins_list() -> CoinsListResponse:
+    async def get_coins_list() -> AsyncGenerator[str]:
         async with httpx.AsyncClient() as client:
-            resp = await client.get(settings.EXTERNAL_URL_COINS_LIST.encoded_string())
-            return resp.json()
+            async with client.stream('GET', settings.EXTERNAL_URL_COINS_LIST.encoded_string()) as resp:
+                resp.raise_for_status()
+                f = ijson.from_iter(resp.aiter_bytes())
+                objects = ijson.items(f, 'item.symbol')
+                symbols = (o async for o in objects)
+                async for symbol in symbols:
+                    yield symbol
