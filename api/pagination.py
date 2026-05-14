87ffca9bebe05dcad446d4793b84b935f1ccee44
Aleksei Khatkevich <qwerty12345@disroot.org>
2026-05-14 16:01:38 +0300

    int2

---
+++ api/pagination.py
@@ -0,0 +1,92 @@
+import dataclasses
+import uuid
+from abc import ABC, abstractmethod
+from typing import Generic, Optional, List, TypeVar
+
+from api.user_assets.domain import UserAssetAggregatedData
+from logic.usecases.user_asset import UserAssetListUseCase
+
+C = TypeVar("C", int, str, uuid.UUID)
+T = TypeVar("T")
+
+
+@dataclasses.dataclass
+class CursorPagination(Generic[C, T]):
+    """Container for data returned using cursor pagination."""
+
+    __slots__ = ("cursor", "items", "next_cursor", "results_per_page", "has_more")
+
+    items: List[T]
+    """List of data being sent as part of the response."""
+    results_per_page: int
+    """Maximal number of items to send."""
+    cursor: Optional[C]  # noqa: UP045
+    """Unique ID, designating the last identifier in the given data set.
+    This value can be used to request the "next" batch of records.
+    """
+    has_more: bool
+    """Whether there are more results available."""
+
+
+class AdvancedCursorPaginator(ABC, Generic[C, T]):
+    """Base cursor paginator that adds ``has_more`` support.
+
+    Subclasses must implement :meth:`get_items` returning
+    ``(items, next_cursor, has_more)``.
+    """
+
+    @abstractmethod
+    async def get_items(
+        self,
+        cursor: C | None,
+        results_per_page: int,
+    ) -> tuple[list[T], C | None, bool]:
+        """Return a page of items following the given cursor.
+
+        Args:
+            cursor: A unique identifier that acts as the 'cursor' after which
+                results should be given.
+            results_per_page: A maximal number of results to return.
+
+        Returns:
+            A tuple containing the result set, a new cursor that marks the
+            last record retrieved, and a boolean indicating whether there
+            are more results.
+        """
+        raise NotImplementedError
+
+    async def __call__(self, cursor: C | None, results_per_page: int) -> CursorPagination[C, T]:
+        """Return a paginated result set given an optional cursor (unique ID)
+        and a maximal number of results to return.
+
+        Args:
+            cursor: A unique identifier that acts as the 'cursor' after which
+                results should be given.
+            results_per_page: A maximal number of results to return.
+
+        Returns:
+            A paginated result set.
+        """
+        items, new_cursor, has_more = await self.get_items(cursor=cursor, results_per_page=results_per_page)
+
+        return CursorPagination[C, T](
+            items=items,
+            results_per_page=results_per_page,
+            cursor=new_cursor,
+            has_more=has_more,
+        )
+
+
+class UserAssetsPaginator(AdvancedCursorPaginator[str, UserAssetAggregatedData]):
+    """Paginator for aggregated user asset data."""
+
+    def __init__(self, user_id: uuid.UUID) -> None:
+        self.user_id = user_id
+
+    async def get_items(
+        self,
+        cursor: str | None,
+        results_per_page: int,
+    ) -> tuple[list[UserAssetAggregatedData], str | None, bool]:
+        data = await UserAssetListUseCase().execute(self.user_id, cursor, results_per_page)
+        return data.items, data.cursor, data.has_more
--- api/user_assets/crud.py
+++ api/user_assets/crud.py
@@ -1,26 +1,22 @@
-import dataclasses
 import uuid
-from typing import Annotated, TypeVar, Generic, Optional, List
 
 from litestar import Controller, route, get
 from litestar.dto import DTOData
 from litestar.openapi import ResponseSpec
-from litestar.openapi.spec import Example
-from litestar.params import Parameter
 from litestar.status_codes import HTTP_400_BAD_REQUEST
 from sqlalchemy.exc import IntegrityError
 
 from api.common_domain import ProblemDetailResponse
 from api.exceptions_handling import integrity_error_handler_factory
-from api.user_assets.domain import UserAssetData, UserAssetDto, UserAssetAggregatedPage, UserAssetAggregatedData
+from api.pagination import CursorPagination, UserAssetsPaginator
+from api.user_assets.domain import UserAssetData, UserAssetDto, UserAssetAggregatedData
 from constants import (
     PG_FOREIGN_KEY_CONSTRAINT_VIOLATION_CODE,
     USER_ASSET_LIST_VIEW_DEFAULT_PAGE_SIZE,
     USER_ASSET_LIST_VIEW_MAX_PAGE_SIZE,
 )
-from logic.usecases.user_asset import UserAssetUpsertUseCase, UserAssetListUseCase
+from logic.usecases.user_asset import UserAssetUpsertUseCase
 from user.domain import User
-from litestar.pagination import AbstractAsyncCursorPaginator, C, T
 
 
 class UserAssetCrudController(Controller):
@@ -52,62 +48,15 @@ class UserAssetCrudController(Controller):
         return user_data
 
 
-
-# todo проверить схему
-# todo caddy opentelemetry
-# todo тесты
+    # todo проверить схему
+    # todo caddy opentelemetry
+    # todo тесты
     @get('/')
     async def get_all_paginated(
             self,
             kc_user: User,
             cursor: str | None,
             results_per_page: int
-    ) -> CursorPagination[str,UserAssetAggregatedData]:
+    ) -> CursorPagination[str, UserAssetAggregatedData]:
         paginator = UserAssetsPaginator(user_id=kc_user.id)
         return await paginator(cursor=cursor, results_per_page=results_per_page)
-
-T = TypeVar("T")
-C = TypeVar("C", int, str, uuid.UUID)
-
-@dataclasses.dataclass
-class CursorPagination(Generic[C, T]):
-    """Container for data returned using cursor pagination."""
-
-    __slots__ = ("cursor", "items", "next_cursor", "results_per_page", "has_more")
-
-    items: List[T]
-    """List of data being sent as part of the response."""
-    results_per_page: int
-    """Maximal number of items to send."""
-    cursor: Optional[C]  # noqa: UP045
-    """Unique ID, designating the last identifier in the given data set.
-    This value can be used to request the "next" batch of records.
-    """
-    has_more: bool
-
-class UserAssetsPaginator(AbstractAsyncCursorPaginator[str, UserAssetAggregatedData]):
-    def __init__(self, user_id: uuid.UUID) -> None:
-        self.user_id = user_id
-
-    async def get_items(self, cursor: C | None, results_per_page: int) -> tuple[list[T], C | None]:
-        data = await UserAssetListUseCase().execute(self.user_id, cursor, results_per_page)
-        return data.items, data.cursor, data.has_more
-
-    async def __call__(self, cursor: C | None, results_per_page: int) -> CursorPagination[C, T]:
-        """Return a paginated result set given an optional cursor (unique ID) and a maximal number of results to return.
-
-        Args:
-            cursor: A unique identifier that acts as the 'cursor' after which results should be given.
-            results_per_page: A maximal number of results to return.
-
-        Returns:
-            A paginated result set.
-        """
-        items, new_cursor, has_more = await self.get_items(cursor=cursor, results_per_page=results_per_page)
-
-        return CursorPagination[C, T](
-            items=items,
-            results_per_page=results_per_page,
-            cursor=new_cursor,
-            has_more=has_more
-        )
